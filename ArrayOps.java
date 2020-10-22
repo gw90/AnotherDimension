c1bcb3575c565747be823ff71b461204fceaccad
Gregory Wickham <gwickham99@gmail.com>
2020-10-22 13:15:07 -0400

    ArrayOps.sum works

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,7 @@
 public class ArrayOps {
-
+    public static int sum(int[] arr) {
+	int c = 0;
+	for(int i = 0; i<arr.length; i++) c+=arr[i];
+	return c;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester {
     public static void main(String[] args){
-
+	int[] a = {1,1,1,1};
+	System.out.println(ArrayOps.sum(a));
     }
 }

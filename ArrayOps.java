cc3d8de2c1bd77f91c0f12d5fea24adfea02ca80
Gregory Wickham <gwickham99@gmail.com>
2020-10-26 11:47:08 -0400

    Added functions

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,4 +9,26 @@ public class ArrayOps {
 	for(int i = 0; i<arr.length; i++) if(arr[i]>l) l=arr[i];
 	return l;
     }
+    public static int[] sumRows(int[][] matrix) {
+	int[] out = new int[matrix.length];
+	for(int i = 0; i<matrix.length; i++) out[i]=sum(matrix[i]);
+	return out;
+    }
+    public static  int[] largestInRows(int[][] matrix){
+	int[] out = new int[matrix.length];
+	for(int i = 0; i<matrix.length; i++) out[i]=largest(matrix[i]);
+	return out;
+    }
+    public static int sum(int[][] arr) {
+	return sum(sumRows(arr));
+    }
+    public static int[] sumCols(int[][] matrix) {
+	int[] out = new int[matrix[0].length];
+	for(int i = 0; i<matrix.length; i++){
+	    for(int j = 0; j<matrix[i].length; j++){
+		out[i]+=matrix[j][i];
+	    }
+	}
+	return out;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,15 @@
 public class Tester {
     public static void main(String[] args){
 	int[] a = {1,3,2,1};
+	int[][] b = {{1,1,1},{2,4,2},{3,-1,-1}};
 	System.out.println(ArrayOps.largest(a));
+	System.out.println(ArrayOps.sumRows(b));
+	System.out.println(ArrayOps.sumRows(b));
+	System.out.println(ArrayOps.largestInRows(b)[1]);
+	System.out.println(ArrayOps.sum(b));
+	System.out.println("--");
+	for(int i = 0; i<ArrayOps.sumCols(b).length; i++){
+	    System.out.println((ArrayOps.sumCols(b)[i]));
+	}
     }
 }
